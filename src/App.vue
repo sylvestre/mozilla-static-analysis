5e135c25e1c12b5b51275adb5d769dd82419ce18
Bastien Abadie <bastien@nextcairn.com>
2018-07-04 13:01:38 +0200

    Add bulma

--- src/App.vue
+++ src/App.vue
@@ -8,14 +8,17 @@ export default {
 </script>
 
 <template>
-  <div :class="$style.App">
-    <h1>Welcome to static-analysis-dashboard</h1>
-yo
-  </div>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">
+        Hello World
+      </h1>
+      <p class="subtitle">
+        My first website with <strong>Bulma</strong>!
+      </p>
+    </div>
+  </section>
 </template>
 
 <style module>
-  .App {
-    padding: 20px;
-  }
 </style>
